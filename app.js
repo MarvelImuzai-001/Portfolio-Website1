136aa79672cd1e6f4cca1afa824cfdf917357dda
Marvel Imuzai <Marvelimuzai@gmail.com>
2024-04-05 00:23:34 +0100

    Portfolio

---
+++ app.js
@@ -0,0 +1,73 @@
+const menu = document.querySelector("#menu");
+const sidebar = document.querySelector(".sidebar");
+
+menu.addEventListener("click", function () {
+  sidebar.style.display = "flex";
+});
+
+const close1 = document.querySelector("#close");
+const sidebar2 = document.querySelector(".sidebar");
+
+close1.addEventListener("click", function () {
+  sidebar2.style.display = "flex";
+});
+
+const button1 = document.querySelector(".btn1");
+
+button1.addEventListener("click", function () {
+  window.open("./documents/cv.pdf");
+});
+
+const button2 = document.querySelector(".btn2");
+
+button2.addEventListener("click", function () {
+  location.href = "./contact.html";
+});
+
+const linkedin = document.querySelector(".linkedin");
+
+linkedin.addEventListener("click", function () {
+  location.href = "https://www.linkedin.com/in/marvel-imuzai-946bba2aa";
+});
+
+const github = document.querySelector(".git");
+
+github.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
+
+const button3 = document.querySelector(".btn3");
+
+button3.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
+
+const button4 = document.querySelector(".btn4");
+
+button4.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
+
+const button5 = document.querySelector(".btn5");
+
+button5.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
+
+const button6 = document.querySelector(".btn6");
+
+button6.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
+
+const button7 = document.querySelector(".btn7");
+
+button7.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
+
+const button8 = document.querySelector(".btn8");
+
+button8.addEventListener("click", function () {
+  location.href = "https://www.github.com/MarvelImuzai-001";
+});
